2dba225e93ad5b37d2de6b7c6166c6141e79c10e
Pavel Brodsky <mcouthon@gmail.com>
2015-04-23 20:19:44 +0300

    Made sure the code is PEP8 compatible

--- core/graph.py
+++ core/graph.py
@@ -1,10 +1,12 @@
 """List of strongly connected components - a list of edges"""
 
+
 class Switch:
     def __init__(self, left, right, top):
         self.left = left
         self.right = right
         self.top = top
+
 
 class Edge:
     def __init__(self, switch1, switch2, cross_half, switch1_index):
@@ -20,20 +22,22 @@ def validate(a, b, n):
     :param n:
     :return: Whether given parameters are as expected
     """
-    if (b != a):
+    if b != a:
         print "Alpha and beta are not consistent"
         return False
-    if (n % 2 == 1):
+    if n % 2 == 1:
         print "N must be even"
         return False
     return True
 
+
 def get_all_switches(n):
     switches = []
     for top in [True, False]:
-        for i in range(n/2):
-            switches.append(Switch(i, n/2 + i, top))
+        for i in range(n / 2):
+            switches.append(Switch(i, n / 2 + i, top))
     return switches
+
 
 def get_containing_switch(switches, ball, top):
     """
@@ -42,7 +46,7 @@ def get_containing_switch(switches, ball, top):
     :return: First switch containing the given ball, at any index
     """
     for switch in switches:
-        if ((switch.left == ball or switch.right == ball) and switch.top == top):
+        if (switch.left == ball or switch.right == ball) and switch.top == top:
             return switch
     return None
 
@@ -54,20 +58,21 @@ def get_list_of_edges(alpha, beta, n):
     :param n:
     :return: List of edges that should be in the switch graph matching the permutation
     """
-    if (not validate(len(alpha), len(alpha), n)):
+    if not validate(len(alpha), len(alpha), n):
         return
 
     switches = get_all_switches(n)
     edges = []
     for i in range(len(alpha)):
         cross_half = 0
-        if ((alpha[i] < n/2 and beta[i] >= n/2) or (alpha[i] >= n/2 and beta[i] < n/2)):
+        if (alpha[i] < n / 2 and beta[i] >= n / 2) or (alpha[i] >= n / 2 and beta[i] < n / 2):
             cross_half = 1
-        switch1_index = 0 if (alpha[i] < n/2) else 1
+        switch1_index = 0 if (alpha[i] < n / 2) else 1
         switch1 = get_containing_switch(switches, alpha[i], True)
         switch2 = get_containing_switch(switches, beta[i], False)
         edges.append(Edge(switch1, switch2, cross_half, switch1_index))
     return edges
+
 
 def get_switch_neighbours(edges, switch):
     """
@@ -77,9 +82,10 @@ def get_switch_neighbours(edges, switch):
     """
     neighbours = []
     for edge in edges:
-        if (edge.switches[0] == switch or edge.switches[1] == switch):
+        if edge.switches[0] == switch or edge.switches[1] == switch:
             neighbours.append(edge)
     return neighbours
+
 
 def extend_last_component(components, current_edge, current_switch):
     """
@@ -87,10 +93,11 @@ def extend_last_component(components, current_edge, current_switch):
     :param current_edge:
     :param current_switch:
     """
-    if (current_edge.cross_half):
+    if current_edge.cross_half:
         components[-1].append([current_edge.switches[current_switch]])
     else:
         components[-1][-1].append(current_edge.switches[current_switch])
+
 
 def get_white_neighbour(edges, switch, blacklist):
     """
@@ -102,12 +109,13 @@ def get_white_neighbour(edges, switch, blacklist):
     neighbours = get_switch_neighbours(edges, switch)
     i = 0
     found_new = False
-    while (not found_new and i < len(neighbours)):
+    while not found_new and i < len(neighbours):
         for j in range(2):
-            if (blacklist.count(neighbours[i].switches[j]) > 0):
+            if blacklist.count(neighbours[i].switches[j]) > 0:
                 found_new = True
         i += 1
-    if (i < len(neighbours)):
+
+    if i < len(neighbours):
         return neighbours[i]
     return None
 
@@ -121,7 +129,7 @@ def find_opening_edge(edges, queue):
     current_switch = 0
     blacklist = []
     next_edge = edges[0]
-    while (next_edge != None):
+    while next_edge:
         blacklist.append(next_edge)
         current_edge = next_edge
         current_switch = 1 - current_switch
@@ -139,21 +147,22 @@ def get_list_of_components(edges, n):
     """
     components = []
     queue = []
-    while (len(edges) > 0 or len(queue) > 0):
-        if (len(queue) == 0):
+    while len(edges) > 0 or len(queue) > 0:
+        if len(queue) == 0:
             current_switch = find_opening_edge(edges, queue)
             start_switch = queue[0].switches[current_switch]
             components.append([[start_switch]])
 
         current_edge = queue.pop()
         current_switch = 1 - current_switch
-        if (current_edge.switches[current_switch] != start_switch):
+        if current_edge.switches[current_switch] != start_switch:
             extend_last_component(components, current_edge, current_switch)
             neighbours = get_switch_neighbours(edges, current_edge.switches[current_switch])
             for neighbour in neighbours:
                 edges.remove(neighbour)
                 queue.insert(0, neighbour)
     return components
+
 
 def get_sccs(alpha, beta, n):
     edges = get_list_of_edges(alpha, beta, n)
