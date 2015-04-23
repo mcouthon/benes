5929459381b1a5ce4eb02de508b0c79cc0702ad2
guydaich <guydaich@mail.tau.ac.il>
2015-04-23 21:43:25 +0300

    add eigenvectors

--- core/matrix.py
+++ core/matrix.py
@@ -1,5 +1,6 @@
 import math
-#import numpy
+import numpy
+from numpy import linalg as LA
 
 class matrix(object):
     def nCr(n,r):
@@ -10,31 +11,72 @@ class matrix(object):
         self.n = n
         self.q = q
         f = math.factorial
-        size = f(n) / f(q) / f(n-q)
+        size = math.floor(f(n) / f(q) / f(n-q))
         self.r = size
         self.c = size
-        self.m = []
+        self.m = numpy.zeros([self.r,self.c],float)
+        for i in range(0,self.r):
+            for j in range(0,self.c):
+                if (i==j):
+                    self.m[i,j] = 1.0;
 
-    def toBaseN(n,t):
+        self.m = numpy.matrix([[1, 2], [2, 1]])
+
+    def ev(self):
+        w, v = LA.eigh(self.m);
+        return w;
+
+    def power(self, p):
+        w, v = LA.eigh(self.m)
+        # eigenvector matrix
+        P = numpy.matrix(v)
+        # Diagonal = (P^t)MP
+        D = numpy.transpose(P) * self.m * P
+        for i in range (0,self.r):
+            D[i,i]=pow(D[i,i],p)
+        D = P * D * numpy.transpose(P)
+        return D
+
+    @staticmethod
+    def fromBaseN(n,t):
+        """
+        :param n: - the base
+        :param t: - tuple representin coordinates in base n
+        :return: - decimal number
+        """
         sum = 0
-        p = n-1
+        p = len(t) - 1
         for i in t:
-            sum += pow(i,p)
-            i = i +1
-            p = p +1
+            sum += i*(pow(n,p))
+            p = p - 1
         return sum
 
-    def fromBaseN(n,q,d):
-        p=n-1
-        l = []
-        for i in range(0,q-1):
-            l[i] = divmod(d,pow(n,q))
-            d = int(d/pow(n,q))
+    @staticmethod
+    def toBaseN(n,q,d):
+        """
+        :param n: base we work in
+        :param q: number of digits in the vector
+        :param d: decimal number to move to new base as tuple
+        :return:
+        """
+        l = [0]*(q)
+        for i in range(0,q):
+            l[i] = int(d%n)
+            d=math.floor(d/n)
         l.reverse()
-        return tuple(list)
+        return tuple(l)
 
- #   def initMatrix(self):
 
+
+def test():
+    m = matrix(2,1)
+    #t = (1,0,0)
+    #print(m.fromBaseN(2,t))
+    #print(m.toBaseN(2,3,4))
+    print(m.power(3))
+
+
+test()
 
 
 
