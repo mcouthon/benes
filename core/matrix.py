0fbd668e2e704787a227d5ba5b214f4ac560fa6a
guydaich <guydaich@mail.tau.ac.il>
2015-05-11 22:04:50 +0300

    add eigenvectors

--- core/matrix.py
+++ core/matrix.py
@@ -1,13 +1,13 @@
 import math
 import numpy
 from numpy import linalg as LA
+import scipy
+#import sympy
+# http://stackoverflow.com/questions/6876377/numpy-arbitrary-precision-linear-algebra
 import calculations
+import itertools
 
 class matrix(object):
-    def nCr(n,r):
-        f = math.factorial
-        return f(n) / f(r) / f(n-r)
-
     def __init__(self, n, q):
         self.n = n
         self.q = q
@@ -15,25 +15,52 @@ class matrix(object):
         size = int(math.floor(f(n) / f(n-q)))
         self.r = size
         self.c = size
-        self.m = numpy.zeros([self.r,self.c],float)
-        print "Matrix Size: " + str(self.r) + "x" + str(self.c)
-        # calculate upper diagonal matrix
-        for i in range(0,self.r):
-            alpha = self.toBaseN(self.n,self.q,i)
-            for j in range(i,self.c):
-                beta = self.toBaseN(self.n,self.q,j)
-                #print alpha + ("->",) + beta
-                self.m[i,j] = calculations.calculate_benes(alpha, beta, n);
-        # fill in symmetric part
-        t = self.m.transpose()
-        numpy.fill_diagonal(t,0)
-        self.m = self.m + t
+        self.m = numpy.zeros([self.r,self.c],numpy.longdouble)
+        self.indicesToVectors = []
+        self.vectorsToIndices = {}
 
-    def ev(self):
+        # build map vector -> matrix index
+        # build map
+        i = 0;
+        for v in itertools.permutations(range(n), q):
+            # build map matrix index -> vector
+            self.indicesToVectors.append(v)
+            # build map vector -> matrix index
+            self.vectorsToIndices[v] = i
+            i = i + 1
+
+        # init matrix with base values
+        for i in range(0,self.r):
+            alpha = self.indicesToVectors[i]
+            for j in range(0,self.c):
+                beta = self.indicesToVectors[j]
+                self.m[i,j] = calculations.calculate_benes_2(alpha, beta, n)
+                if self.checkProbQ2(self.m[i,j]) is False:
+                    print self.m[i,j]
+        # fill in symmetric part
+        #t = self.m.transpose()
+        #numpy.fill_diagonal(t,0)
+        #self.m = self.m + t
+
+    def getProbAlphaBeta(self, alpha, beta):
+        i = self.vectorsToIndices(alpha)
+        j = self.vectorsToIndices(beta)
+        return self.m[i,j]
+
+    def getEigenvalues(self):
         w, v = LA.eigh(self.m);
         return w;
 
-    def power(self, p):
+    def getDiagonal(self):
+        w, v = LA.eigh(self.m)
+        # eigenvector matrix
+        P = numpy.matrix(v)
+        # Diagonal = (P^t)MP
+        D = numpy.transpose(P) * self.m * P
+        return D
+
+    # exponentiate matrix
+    def getMatrixPower(self, p):
         w, v = LA.eigh(self.m)
         # eigenvector matrix
         P = numpy.matrix(v)
@@ -43,6 +70,19 @@ class matrix(object):
             D[i,i]=pow(D[i,i],p)
         D = P * D * numpy.transpose(P)
         return D
+
+    def checkProbQ2(self, p):
+        probs = [float(pow(2.0,float(i))/pow(8.0,3.0) + (1.0/pow(8.0,2.0))) for i in range(1,4)]
+        probs.append(float(1.0/(pow(8.0,2.0))))
+        if (p in probs):
+            return True
+        return False
+
+    def getEigenvalueSet(self):
+        return set(self.getEigenvalues())
+
+    def getRoundEigevalueSet(self):
+        return set(numpy.round(self.getEigenvalues(),14))
 
     @staticmethod
     def fromBaseN(n,t):
@@ -81,14 +121,21 @@ class matrix(object):
         j = self.fromBaseN(self.n,beta)
         return self.m[i,j]
 
+    def findUnsymPair(self):
+        for i in range(0,self.r):
+            for j in range(0,self.c):
+                if (self.m[i,j] != self.m[j,i]):
+                    print self.toBaseN(self.n, self.q, i), ("->",), self.toBaseN(self.n, self.q, j)
 
 
 def test():
     m = matrix(8,2)
-    print m.m
-    print m.isSymmetric()
-    print m.ev()
+    #print m.isSymmetric()
     #print(m.power(3))
+    #print m.getDiagonal()
+    print m.getEigenvalues()
+    #print m.getEigenvalueSet()
+    print m.getRoundEigevalueSet()
 test()
 
 
